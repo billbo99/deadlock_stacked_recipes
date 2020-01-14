431f5d31b6b1d69e0ade338f4e7658a8d0213208
billbo99 <cactus_computers-github@yahoo.com>
2020-01-14 21:59:33 +0000

    If beltboxes not enabled, then stop this mod from taking any action.

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,7 +1,9 @@
-local Deadlock = require("utils/deadlock")
+if settings.startup["deadlock-enable-beltboxes"].value then
+    local Deadlock = require("utils/deadlock")
 
-Deadlock.DensityOverride()
-Deadlock.MakeStackedRecipes()
-Deadlock.DensityOverride()
-Deadlock.FixLocalisedNames()
-Deadlock.SubGroups()
+    Deadlock.DensityOverride()
+    Deadlock.MakeStackedRecipes()
+    Deadlock.DensityOverride()
+    Deadlock.FixLocalisedNames()
+    Deadlock.SubGroups()
+end
--- data.lua
+++ data.lua
@@ -1,8 +1,14 @@
 log("data-start")
-require("data/group.lua")
-require("mods/vanilla/data.lua")
-require("mods/DyWorld/data.lua")
-require("mods/Krastorio/data.lua")
-require("mods/FactorioExtended-Plus-Core/data.lua")
-require("mods/FactorioExtended-Plus-Revisited/data.lua")
+
+if settings.startup["deadlock-enable-beltboxes"].value then
+    require("data/group.lua")
+    require("mods/vanilla/data.lua")
+    require("mods/DyWorld/data.lua")
+    require("mods/Krastorio/data.lua")
+    require("mods/FactorioExtended-Plus-Core/data.lua")
+    require("mods/FactorioExtended-Plus-Revisited/data.lua")
+else
+    log("!! DeadLock BeltBoxes not enabled, this mod has nothing to do without them.")
+end
+
 log("data-end")
