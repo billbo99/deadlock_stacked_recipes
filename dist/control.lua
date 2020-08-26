c2ef4c2162fc93d334ce1a71f67c85fe58357cd9
billbo99 <cactus_computers-github@yahoo.com>
2020-08-26 15:09:16 +0100

    1.0.1

---
+++ dist/control.lua
@@ -0,0 +1,25 @@
+local Func = require("utils.func")
+
+script.on_configuration_changed(
+    function(e)
+        -- e.mod_changes
+        -- e.mod_startup_settings_changed
+        -- e.migration_applied
+        log("on_configuration_changed")
+        if e.mod_startup_settings_changed or e.mod_changes["deadlock_stacked_recipes"] then
+            for index, force in pairs(game.forces) do
+                local recipes = force.recipes
+                for _, tech in pairs(force.technologies) do
+                    if tech.researched then
+                        for _, effect in pairs(tech.effects) do
+                            if effect.type == "unlock-recipe" and Func.starts_with(effect.recipe, "StackedRecipe") then
+                                recipes[effect.recipe].enabled = true
+                                recipes[effect.recipe].reload()
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
+)
