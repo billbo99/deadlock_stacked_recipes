76b5e67d738ca7d01943d9116ba4dae2409eb250
billbo99 <cactus_computers-github@yahoo.com>
2020-09-24 08:40:52 +0100

    1.0.6

--- dist/mods/Kythbloods_Stacked_Mining/data-final-fixes.lua
+++ dist/mods/Kythbloods_Stacked_Mining/data-final-fixes.lua
@@ -14,7 +14,18 @@ end
 
 for recipe, recipe_table in pairs(data.raw.recipe) do
     if Func.starts_with(recipe, "mine-deadlock-stack") then
-        local item_stack_size = data.raw.item[recipe_table.results[1].name].stack_size
-        recipe_table.results[1].amount = math.min(item_stack_size * 100, (2 ^ 16) - 1)
+        local item_stack_size
+        if recipe_table.results then
+            item_stack_size = data.raw.item[recipe_table.results[1].name].stack_size
+            recipe_table.results[1].amount = math.min(item_stack_size * 100, (2 ^ 16) - 1)
+        end
+        if recipe_table.normal.results then
+            item_stack_size = data.raw.item[recipe_table.normal.results[1].name].stack_size
+            recipe_table.normal.results[1].amount = math.min(item_stack_size * 100, (2 ^ 16) - 1)
+        end
+        if recipe_table.expensive.results then
+            item_stack_size = data.raw.item[recipe_table.expensive.results[1].name].stack_size
+            recipe_table.expensive.results[1].amount = math.min(item_stack_size * 100, (2 ^ 16) - 1)
+        end
     end
 end
